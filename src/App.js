24866b2183a840515e57593e007b2925879254e9
Bruce Dong <yldbyldb@gmail.com>
2020-09-27 13:49:12 +1000

    updating of preparing to setup the function and rst contact-us

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from "./App.module.scss";
 import { BrowserRouter, Route, Link } from "react-router-dom";
-import ContactUsClass from "./pages/ContactUs/ContactUs";
+import ContactUsClass from "./pages/ContactUs/ContactUsClass";
 import MedianPrice from "./pages/home/MedianPrice";
 
 function App() {
