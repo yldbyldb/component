0bd24b2a8447d12fdd00313a7c208e9a504c3560
Bruce Dong <yldbyldb@gmail.com>
2020-09-27 13:58:43 +1000

    updating of preparing to setup the function, rst and formik contact-us

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import classes from "./App.module.scss";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import ContactUsClass from "./pages/ContactUs/ContactUsClass";
+import ContactUsFunction from "./pages/ContactUs/ContactUsFunction";
+import ContactUsRst from "./pages/ContactUs/ContactUsRst";
+import ContactUsFormik from "./pages/ContactUs/ContactUsFormik";
 import MedianPrice from "./pages/home/MedianPrice";
 
 function App() {
@@ -14,12 +17,24 @@ function App() {
               <Link to="/ContactUsClass">Contact Us (Class)</Link>
             </li>
             <li>
+              <Link to="/ContactUsFunction">Contact Us (Function)</Link>
+            </li>
+            <li>
+              <Link to="/ContactUsRst">Contact Us (Rst)</Link>
+            </li>
+            <li>
+              <Link to="/ContactUsFormik">Contact Us (Formik)</Link>
+            </li>
+            <li>
               <Link to="/MedianPrice">Median Price</Link>
             </li>
           </ul>
         </nav>
         <div>
           <Route path="/ContactUsClass" component={ContactUsClass}></Route>
+          <Route path="/ContactUsFunction" component={ContactUsFunction}></Route>
+          <Route path="/ContactUsRst" component={ContactUsRst}></Route>
+          <Route path="/ContactUsFormik" component={ContactUsFormik}></Route>
           <Route path="/MedianPrice" component={MedianPrice}></Route>
         </div>
       </BrowserRouter>
