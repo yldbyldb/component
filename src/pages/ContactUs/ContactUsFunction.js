207e6e396c4c39a0e644ec5b5bb1419441ea5b3e
Bruce Dong <yldbyldb@gmail.com>
2020-09-27 15:47:40 +1000

    add contactusfunction

--- src/pages/ContactUs/ContactUsFunction.js
+++ src/pages/ContactUs/ContactUsFunction.js
@@ -1,124 +1,86 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Field from "./Field.js";
 import Button from "./Button.js";
 import classes from "./ContactUs.module.scss";
 
-class ContactUsClass extends Component {
-  constructor() {
-    super();
-    this.state = {
-      firstName: "",
-      isFirstNameErr: false,
-      firstNameErr: "",
+const ContactUsFunction = () => {
+  const [firstName, setFirstName] = useState("");
+  const [isFirstNameErr, setIsFirstNameErr] = useState(false);
+  const [firstNameErr, setFirstNameErr] = useState("");
 
-      lastName: "",
-      isLastNameErr: false,
-      lastNameErr: "",
+  const [lastName, setLastName] = useState("");
+  const [isLastNameErr, setIsLastNameErr] = useState(false);
+  const [lastNameErr, setLastNameErr] = useState("");
 
-      email: "",
-      isEmailErr: false,
-      emailErr: "",
+  const [email, setEmail] = useState("");
+  const [isEmailErr, setIsEmailErr] = useState(false);
+  const [emailErr, setEmailErr] = useState("");
 
-      message: "",
-      isMessageErr: false,
-      messageErr: "",
+  const [message, setMessage] = useState("");
+  const [isMessageErr, setIsMessageErr] = useState(false);
+  const [messageErr, setMessageErr] = useState("");
 
-      isClicked: false,
-    };
-  }
+  const [isClicked, setIsClicked] = useState(false);
 
-  updateField = (field, value) => {
-    this.setState({ [field]: value });
-  };
+  //   const updateField = (field, value) => {
+  //     this.setState({ [field]: value });
+  //   };
 
-  errHandler = (field, fieldErr, isFieldErr) => {
-    if (this.state[fieldErr]) {
-      this.setState({
-        [field]: "",
-        isClicked: false,
-        [fieldErr]: "",
-        [isFieldErr]: false,
-      });
+  const errHandler = (fieldErr, setField, setFieldErr, setIsFieldErr) => {
+    if (fieldErr) {
+      [setField]("");
+      setIsClicked(false);
+      [setFieldErr]("");
+      [setIsFieldErr](false);
     }
   };
 
-  validFirstName = () => {
-    const { firstName } = this.state;
+  const validFirstName = () => {
     if (firstName.trim() === "" || firstName.length < 2) {
-      this.setState({
-        isFirstNameErr: true,
-        firstNameErr: "Please enter your first name.",
-      });
+      setIsFirstNameErr(true);
+      setFirstNameErr("Please enter your first name.");
     }
   };
-  validLastName = () => {
-    const { lastName } = this.state;
+  const validLastName = () => {
     if (lastName.trim() === "" || lastName.length < 2) {
-      this.setState({
-        isLastNameErr: true,
-        lastNameErr: "Please enter your last name.",
-      });
+      setIsLastNameErr(true);
+      setLastNameErr("Please enter your last name.");
     }
   };
-  validEmail = () => {
-    const { email } = this.state;
+  const validEmail = () => {
     const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if (email.trim() === "" || email.length < 5) {
-      this.setState({
-        isEmailErr: true,
-        emailErr: "email must be 5 or more characters >_<",
-      });
+      setIsEmailErr(true);
+      setEmailErr("email must be 5 or more characters >_<");
       console.log("blur!");
     } else if (email.indexOf("@") === -1) {
-      this.setState({
-        isEmailErr: true,
-        emailErr: "Where is the @ symbol (-_-)b",
-      });
+      setIsEmailErr(true);
+      setEmailErr("Where is the @ symbol (-_-)b");
     } else if (email.indexOf("@") !== email.lastIndexOf("@")) {
-      this.setState({
-        isEmailErr: true,
-        emailErr: "One @ is perfect, two is too many (^_^)",
-      });
+      setIsEmailErr(true);
+      setEmailErr("One @ is perfect, two is too many (^_^)");
     } else if (email.indexOf(".") === -1) {
-      this.setState({
-        isEmailErr: true,
-        emailErr: "Where is the . (-_-)b",
-      });
+      setIsEmailErr(true);
+      setEmailErr("Where is the . (-_-)b");
     } else if (email.indexOf("@") > email.lastIndexOf(".")) {
-      this.setState({
-        isEmailErr: true,
-        emailErr:
-          "@ and . are two different things. En, you should swap them with each other (-_-)b",
-      });
+      setIsEmailErr(true);
+      setEmailErr(
+        "@ and . are two different things. En, you should swap them with each other (-_-)b"
+      );
     } else if (!emailRegex.test(email)) {
-      this.setState({
-        isEmailErr: true,
-        emailErr: "Email does not appear to be valid.",
-      });
+      setIsEmailErr(true);
+      setEmailErr("Email does not appear to be valid.");
     }
   };
-  validMessage = () => {
-    const { message } = this.state;
+  const validMessage = () => {
     if (message.trim() === "" || message.length < 10) {
-      this.setState({
-        isMessageErr: true,
-        messageErr: "Please say something more o(^_^)o",
-      });
+      setIsMessageErr(true);
+      setMessageErr("Please say something more o(^_^)o");
     }
   };
 
-  validAll = () => {
-    const {
-      isFirstNameErr,
-      isLastNameErr,
-      isEmailErr,
-      isMessageErr,
-      firstName,
-      lastName,
-      email,
-      message,
-    } = this.state;
+  const validAll = () => {
     if (
       !isFirstNameErr &&
       !isLastNameErr &&
@@ -132,116 +94,130 @@ class ContactUsClass extends Component {
       window.location.href = `mailto:#?subject=Interested%20Client&body=${this.state.message}`;
     }
     if (firstName === "") {
-      this.setState({
-        isFirstNameErr: true,
-        firstNameErr: "Please enter your first name.",
-      });
+      setIsFirstNameErr(true);
+      setFirstNameErr("Please enter your first name.");
     }
     if (lastName === "") {
-      this.setState({
-        isLastNameErr: true,
-        lastNameErr: "Please enter your last name.",
-      });
+      setIsLastNameErr(true);
+      setLastNameErr("Please enter your last name.");
     }
     if (email === "") {
-      this.setState({
-        isEmailErr: true,
-        emailErr: "email must be 5 or more characters >_<",
-      });
+      setIsEmailErr(true);
+      setEmailErr("email must be 5 or more characters >_<");
     }
     if (message === "") {
-      this.setState({
-        isMessageErr: true,
-        messageErr: "Please say something more o(^_^)o",
-      });
+      setIsMessageErr(true);
+      setMessageErr("Please say something more o(^_^)o");
     }
   };
 
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    this.setState(
-      {
-        isClicked: true,
-      },
-      () => console.log(this.state.isClicked)
-    );
-    this.validAll();
+    setIsClicked(true);
+    console.log(isClicked);
+    validAll();
   };
 
-  render() {
-    return (
-      <div className={classes.Wrapper}>
-        <h2>Contact Us (Class)</h2>
+  return (
+    <div className={classes.Wrapper}>
+      <h2>Contact Us (Function)</h2>
 
-        <div className={classes.Form}>
-          <Field
-            label="First Name"
-            onChange={(e) => this.updateField("firstName", e.target.value)}
-            value={this.state.isFirstNameErr ? "" : this.state.firstName}
-            onBlur={this.validFirstName}
-            error={this.state.firstNameErr}
-            isError={this.state.isFirstNameErr}
-            onClick={() =>
-              this.errHandler("firstName", "firstNameErr", "isFirstNameErr")
-            }
-          />
-          <Field
-            label="Last Name"
-            onChange={(e) => this.updateField("lastName", e.target.value)}
-            value={this.state.isLastNameErr ? "" : this.state.lastName}
-            onBlur={this.validLastName}
-            error={this.state.lastNameErr}
-            isError={this.state.isLastNameErr}
-            onClick={() =>
-              this.errHandler("lastName", "lastNameErr", "isLastNameErr")
-            }
-          />
-
-          <Field
-            label="Email"
-            onChange={(e) => this.updateField("email", e.target.value)}
-            value={this.state.isEmailErr ? "" : this.state.email}
-            onBlur={this.validEmail}
-            error={this.state.emailErr}
-            isError={this.state.isEmailErr}
-            onClick={() => this.errHandler("email", "emailErr", "isEmailErr")}
-          />
-
-          {this.state.isMessageErr ? (
-            <div className={classes.Red}>
-              <textarea
-                onChange={(e) => this.updateField("message", e.target.value)}
-                value={this.state.isMessageErr ? "" : this.state.message}
-                onBlur={this.validMessage}
-                onClick={() =>
-                  this.errHandler("message", "messageErr", "isMessageErr")
-                }
-                placeholder={this.state.messageErr}
-              ></textarea>
-            </div>
-          ) : (
-            <div className={classes.Blu}>
-              <textarea
-                onChange={(e) => this.updateField("message", e.target.value)}
-                value={this.state.isMessageErr ? "" : this.state.message}
-                onBlur={this.validMessage}
-                onClick={() =>
-                  this.errHandler("message", "messageErr", "isMessageErr")
-                }
-                placeholder={"Your Message Please"}
-              ></textarea>
-            </div>
-          )}
-        </div>
-
-        <Button
-          // formValues={this.state}
-          onClick={this.onSubmit}
-          isClicked={this.state.isClicked}
+      <div className={classes.Form}>
+        <Field
+          label="First Name"
+          onChange={(e) => setFirstName(e.target.value)}
+          value={isFirstNameErr ? "" : firstName}
+          onBlur={validFirstName}
+          error={firstNameErr}
+          isError={isFirstNameErr}
+          onClick={() =>
+            //   {if (firstNameErr){
+            //     setFirstName("");
+            //     setIsClicked(false);
+            //     setFirstNameErr("");
+            //     setIsFirstNameErr(false);
+            //   }}
+            errHandler(
+              firstNameErr,
+              "setFirstName",
+              "setFirstNameErr",
+              "setIsFirstNameErr"
+            )
+          }
         />
-      </div>
-    );
-  }
-}
+        <Field
+          label="Last Name"
+          onChange={(e) => setLastName(e.target.value)}
+          value={isLastNameErr ? "" : lastName}
+          onBlur={validLastName}
+          error={lastNameErr}
+          isError={isLastNameErr}
+          onClick={() =>
+            errHandler(
+              lastNameErr,
+              "setLastName",
+              "setLastNameErr",
+              "setIsLastNameErr"
+            )
+          }
+        />
 
-export default ContactUsClass;
+        <Field
+          label="Email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={isEmailErr ? "" : email}
+          onBlur={validEmail}
+          error={emailErr}
+          isError={isEmailErr}
+          onClick={() =>
+            errHandler(emailErr, "setEmail", "setEmailErr", "setIsEmailErr")
+          }
+        />
+
+        {isMessageErr ? (
+          <div className={classes.Red}>
+            <textarea
+              onChange={(e) => setMessage(e.target.value)}
+              value={isMessageErr ? "" : message}
+              onBlur={validMessage}
+              onClick={() =>
+                errHandler(
+                  messageErr,
+                  "setMessage",
+                  "setMessageErr",
+                  "setIsMessageErr"
+                )
+              }
+              placeholder={messageErr}
+            ></textarea>
+          </div>
+        ) : (
+          <div className={classes.Blu}>
+            <textarea
+              onChange={(e) => setMessage(e.target.value)}
+              value={isMessageErr ? "" : message}
+              onBlur={validMessage}
+              onClick={() =>
+                errHandler(
+                  messageErr,
+                  "setMessage",
+                  "setMessageErr",
+                  "setIsMessageErr"
+                )
+              }
+              placeholder={"Your Message Please"}
+            ></textarea>
+          </div>
+        )}
+      </div>
+
+      <Button
+        // formValues={this.state}
+        onClick={onSubmit}
+        isClicked={isClicked}
+      />
+    </div>
+  );
+};
+
+export default ContactUsFunction;
